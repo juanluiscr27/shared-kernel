5c62ab42a44f434bd48c9cc7a5c2c02a7f4224a9
Juan Luis Casanova <juanluiscr27@hotmail.com>
2024-04-21 17:48:10 -0400

    Added test for `ProblemDetail`. Changed json dump method on `ErrorResponse` test

--- tests/api/contracts_test.py
+++ tests/api/contracts_test.py
@@ -1,10 +1,28 @@
+import json
 from dataclasses import dataclass
 
 from sharedkernel.api.contracts import ErrorResponse, ProblemDetail
 from sharedkernel.domain.errors import Error, UnknownEvent
 
 
-def test_problem_detail_is_constructed_with_error():
+def test_problem_detail_is_parsed_to_json():
+    # Arrange
+    error_dict = {'loc': ["Users.CreateUser"],
+                  'msg': "First name is null or empty.",
+                  'type': "FirstName.NullOrEmpty", }
+
+    expected = json.dumps(error_dict, separators=(',', ':'))
+
+    # Act
+    result = ProblemDetail(loc=["Users.CreateUser"],
+                           msg="First name is null or empty.",
+                           type="FirstName.NullOrEmpty", )
+
+    # Assert
+    assert result.model_dump_json() == expected
+
+
+def test_problem_detail_is_constructed_with_error_response():
     # Arrange
     expected = ProblemDetail(loc=["Users.CreateUser"],
                              msg="First name is null or empty.",
@@ -20,5 +38,4 @@ def test_problem_detail_is_constructed_with_error():
     result = ErrorResponse(error=null_or_empty)
 
     # Assert
-    assert result.json() == expected.json()
-
+    assert result.model_dump_json() == expected.model_dump_json()
