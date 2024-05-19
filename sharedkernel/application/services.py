e4f97cc16e55f58d1857a4364a11268d93b27be8
Juan Luis Casanova <juanluiscr27@hotmail.com>
2024-05-19 18:38:52 -0400

    Add service bus implementation
    Defined method to register handlers.
    Defined method to send requests and return responses.

--- sharedkernel/application/services.py
+++ sharedkernel/application/services.py
@@ -1,2 +1,142 @@
+import typing
+from abc import ABC, abstractmethod
+from types import get_original_bases
+from typing import Dict, TypeVar, Union
+
+from result import Ok, Err
+
+from sharedkernel.application.commands import Command, CommandHandler, Acknowledgement
+from sharedkernel.application.errors import HandlerAlreadyRegistered, Rejection, UnsupportedHandler, ServiceBusErrors
+from sharedkernel.application.queries import Query, QueryHandler, TResult
+from sharedkernel.application.validators import Validator, ValidationResult, TRequest
+from sharedkernel.domain.data import ReadModel, ReadModelList
+from sharedkernel.domain.errors import DomainError
+
+
 class ApplicationService:
     """Application Service marker class"""
+
+
+THandler = TypeVar("THandler", bound=CommandHandler | QueryHandler)
+TResponse = TypeVar("TResponse", bound=Acknowledgement | ReadModel | ReadModelList)
+
+
+class Sender(ABC):
+    """Request Sender Interface"""
+
+    @abstractmethod
+    def register(self, handler: Union[THandler, Validator]) -> bool:
+        """Register a handler to process a request when a message is sent.
+
+            Args:
+                handler: Can be a Command Handler, a Query Handler or a Validator.
+            """
+        ...
+
+    @abstractmethod
+    def send(self, request: TRequest) -> Union[TResponse, Rejection]:
+        """Routes a request to a particular handler.
+
+            Args:
+                request: Can be a Command or Query Handler.
+            """
+        ...
+
+
+class ServiceBus:
+    """Service Bus
+
+    Communication mechanism used to decouple the sender of a message from its handler.
+    Supports Commands, Queries, and Validators.
+    Service Bus sends Request and returns Response.
+    """
+
+    def __init__(self) -> None:
+        self._handlers: Dict[str, THandler] = {}
+        self._validators: Dict[str, Validator] = {}
+
+    def register(self, handler: Union[THandler, Validator]) -> bool:
+        bases = get_original_bases(handler.__class__)
+        args = typing.get_args(bases[0])
+        request_type = args[0].__name__
+
+        if request_type in self._handlers:
+            raise HandlerAlreadyRegistered(self, request_type)
+
+        if isinstance(handler, CommandHandler | QueryHandler):
+            self._handlers[request_type] = handler
+            return True
+
+        if isinstance(handler, Validator):
+            self._validators[request_type] = handler
+            return True
+
+        handler_type = type(handler).__name__
+        raise UnsupportedHandler(self, handler_type)
+
+    def send(self, request: TRequest) -> Union[TResponse, Rejection]:
+        try:
+            return self.process(request)
+        except DomainError as error:
+            return Rejection.from_exception(status=422, error=error)
+
+    def process(self, request: TRequest) -> TResponse:
+        validation_result = self.pre_process(request)
+
+        if not validation_result.is_valid:
+            return Rejection.from_validation(validation_result)
+
+        if isinstance(request, Command):
+            return self.process_command(request)
+
+        if isinstance(request, Query):
+            return self.process_query(request)
+
+        request_type = type(request).__name__
+        error = ServiceBusErrors.request_not_supported(request_type)
+
+        return Rejection.from_error(status=501, error=error)
+
+    def pre_process(self, request: TRequest) -> ValidationResult:
+        request_type = type(request).__name__
+
+        if request_type not in self._validators:
+            return ValidationResult.success()
+
+        validators = self._validators[request_type]
+
+        return validators.validate(request)
+
+    def process_command(self, command: Command) -> Union[Acknowledgement, Rejection]:
+        command_type = type(command).__name__
+
+        if command_type not in self._handlers:
+            error = ServiceBusErrors.no_handler_registered_for_request(command_type)
+            return Rejection.from_error(status=501, error=error)
+
+        handler = self._handlers[command_type]
+
+        result = handler.execute(command)
+
+        match result:
+            case Ok(ack):
+                return ack
+            case Err(error):
+                return Rejection.from_error(status=422, error=error)
+
+    def process_query(self, query: Query) -> Union[TResult, Rejection]:
+        query_type = type(query).__name__
+
+        if query_type not in self._handlers:
+            error = ServiceBusErrors.no_handler_registered_for_request(query_type)
+            return Rejection.from_error(status=501, error=error)
+
+        handler = self._handlers[query_type]
+
+        result = handler.execute(query)
+
+        match result:
+            case Ok(read_model):
+                return read_model
+            case Err(error):
+                return Rejection.from_error(status=404, error=error)
---
+++ tests/application/service_bus_test.py
@@ -0,0 +1,366 @@
+from dataclasses import dataclass
+from uuid import UUID
+
+import pytest
+from result import Result, Ok, Err
+
+from sharedkernel.application.commands import Command, CommandHandler, Acknowledgement, CommandStatus
+from sharedkernel.application.errors import UnsupportedHandler
+from sharedkernel.application.queries import Query, QueryHandler
+from sharedkernel.application.services import ServiceBus
+from sharedkernel.application.validators import Validator, ValidationResult
+from sharedkernel.domain.data import ReadModel
+from sharedkernel.domain.errors import Error
+from sharedkernel.domain.events import DomainEvent, DomainEventHandler
+
+
+@dataclass(frozen=True)
+class UserModel(ReadModel):
+    user_id: UUID
+    name: str
+    slug: str
+
+
+@dataclass(frozen=True)
+class RegisterUser(Command):
+    user_id: UUID
+    name: str
+    slug: str
+
+
+@dataclass(frozen=True)
+class GetUserByID(Query):
+    user_id: UUID
+
+
+def name_null_or_empty_error() -> Error:
+    return Error(
+        message="User name is null or empty.",
+        code="User.Name.NullOrEmpty",
+        reason=f"User 'name' should not be null nor empty.",
+        domain="Test.Users.RegisterUser",
+    )
+
+
+def slug_not_unique_error(slug: str) -> Error:
+    return Error(
+        message="User slug is not unique.",
+        code="User.Slug.NotUnique",
+        reason=f"User slug '{slug}' already exists.",
+        domain="Test.Users.RegisterUser",
+    )
+
+
+def id_not_found(user_id: str) -> Error:
+    return Error(
+        message=f"User id '{user_id} not found'.",
+        code="User.Id.NotFound",
+        reason=f"User 'ID' is not available.",
+        domain="Test.users.GetUserById",
+    )
+
+
+class RegisterUserCommandHandler(CommandHandler[RegisterUser]):
+
+    def execute(self, command: RegisterUser) -> Result[Acknowledgement, Error]:
+        if command.user_id == UUID('018f9284-769b-726d-b3bf-3885bf2ddd3c'):
+            ack = Acknowledgement(
+                status=CommandStatus.RECEIVED,
+                action="RegisterUser",
+                entity_id=UUID('018f9284-769b-726d-b3bf-3885bf2ddd3c'),
+                position=1, )
+            return Ok(ack)
+        else:
+            error = slug_not_unique_error(command.slug)
+            return Err(error)
+
+
+class GetUserByIDQueryHandler(QueryHandler[GetUserByID]):
+
+    def execute(self, command: GetUserByID) -> Result[ReadModel, Error]:
+        if command.user_id == UUID('018f9284-769b-726d-b3bf-3885bf2ddd3c'):
+            user = UserModel(
+                user_id=UUID('018f9284-769b-726d-b3bf-3885bf2ddd3c'),
+                name="John Doe Smith",
+                slug="john-doe-smith",
+            )
+            return Ok(user)
+        else:
+            error = id_not_found(str(command.user_id))
+            return Err(error)
+
+
+class RegisterOfficialValidator(Validator[RegisterUser]):
+
+    def validate(self, request: RegisterUser) -> ValidationResult:
+        errors = []
+
+        if request.name is None or request.name.strip() == "":
+            name_null_or_empty = name_null_or_empty_error()
+            errors.append(name_null_or_empty)
+        if errors:
+            return ValidationResult.with_errors(errors=errors)
+        else:
+            return ValidationResult.success()
+
+
+@dataclass(frozen=True)
+class UserRegistered(DomainEvent):
+    event_id: str
+    message: str
+
+
+class RegistrationEventHandler(DomainEventHandler[UserRegistered]):
+
+    def process(self, event: UserRegistered):
+        pass
+
+
+def test_command_handler_is_registered_to_service_bus():
+    # Arrange
+    bus = ServiceBus()
+    handler = RegisterUserCommandHandler()
+
+    # Act
+    result = bus.register(handler)
+
+    # Assert
+    assert result is True
+
+
+def test_query_handler_is_registered_to_service_bus():
+    # Arrange
+    bus = ServiceBus()
+    validator = RegisterOfficialValidator()
+
+    # Act
+    result = bus.register(validator)
+
+    # Assert
+    assert result is True
+
+
+def test_validator_is_registered_to_service_bus():
+    # Arrange
+    bus = ServiceBus()
+    handler = GetUserByIDQueryHandler()
+
+    # Act
+    result = bus.register(handler)
+
+    # Assert
+    assert result is True
+
+
+def test_registering_event_handler_service_bus_raise_error():
+    # Arrange
+    expected = "`RegistrationEventHandler` cannot be registered in the Service Bus"
+
+    bus = ServiceBus()
+    handler = RegistrationEventHandler()
+
+    # Act
+    with pytest.raises(UnsupportedHandler) as error:
+        # noinspection PyTypeChecker
+        _ = bus.register(handler)
+
+    # Assert
+    assert str(error.value) == expected
+
+
+def test_pre_process_request_with_no_validator_return_valid_result():
+    # Arrange
+    bus = ServiceBus()
+    command = RegisterUser(
+        user_id=UUID('018f9284-769b-726d-b3bf-3885bf2ddd3c'),
+        name="John Doe Smith",
+        slug="john-doe-smith",
+    )
+
+    # Act
+    result = bus.pre_process(command)
+
+    # Assert
+    assert result.is_valid is True
+
+
+def test_pre_process_valid_request_return_valid_result():
+    # Arrange
+    bus = ServiceBus()
+    validator = RegisterOfficialValidator()
+    bus.register(validator)
+
+    command = RegisterUser(
+        user_id=UUID('018f9284-769b-726d-b3bf-3885bf2ddd3c'),
+        name="John Doe Smith",
+        slug="john-doe-smith",
+    )
+
+    # Act
+    result = bus.pre_process(command)
+
+    # Assert
+    assert result.is_valid is True
+
+
+def test_pre_process_invalid_request_return_validation_errors():
+    # Arrange
+    bus = ServiceBus()
+    validator = RegisterOfficialValidator()
+    bus.register(validator)
+
+    command = RegisterUser(
+        user_id=UUID('018f9284-769b-726d-b3bf-3885bf2ddd3c'),
+        name="",
+        slug="john-doe-smith",
+    )
+
+    # Act
+    result = bus.pre_process(command)
+
+    # Assert
+    assert result.is_valid is False
+
+
+def test_process_command_with_handler_return_rejection():
+    # Arrange
+    expected = 501
+    bus = ServiceBus()
+
+    command = RegisterUser(
+        user_id=UUID('018f9284-769b-726d-b3bf-3885bf2ddd3c'),
+        name="John Doe Smith",
+        slug="john-doe-smith",
+    )
+
+    # Act
+    result = bus.process_command(command)
+
+    # Assert
+    assert result.status == expected
+
+
+def test_process_valid_command_return_command_acknowledgment():
+    # Arrange
+    bus = ServiceBus()
+    handler = RegisterUserCommandHandler()
+    bus.register(handler)
+
+    command = RegisterUser(
+        user_id=UUID('018f9284-769b-726d-b3bf-3885bf2ddd3c'),
+        name="John Doe Smith",
+        slug="john-doe-smith",
+    )
+
+    # Act
+    result = bus.process_command(command)
+
+    # Assert
+    assert result.status is CommandStatus.RECEIVED
+
+
+def test_process_invalid_command_return_rejection():
+    # Arrange
+    expected = 422
+    bus = ServiceBus()
+    handler = RegisterUserCommandHandler()
+    bus.register(handler)
+
+    command = RegisterUser(
+        user_id=UUID('018f928b-5546-77e6-badf-3155de144924'),
+        name="John Doe Smith",
+        slug="john-doe-smith",
+    )
+
+    # Act
+    result = bus.process_command(command)
+
+    # Assert
+    assert result.status == expected
+
+
+def test_process_query_with_handler_return_rejection():
+    # Arrange
+    expected = 501
+    user_id = UUID('018f9284-769b-726d-b3bf-3885bf2ddd3c')
+
+    bus = ServiceBus()
+
+    query = GetUserByID(user_id=user_id)
+
+    # Act
+    result = bus.process_query(query)
+
+    # Assert
+    assert result.status == expected
+
+
+def test_process_valid_query_return_read_model():
+    # Arrange
+    user_id = UUID('018f9284-769b-726d-b3bf-3885bf2ddd3c')
+
+    bus = ServiceBus()
+    handler = GetUserByIDQueryHandler()
+    bus.register(handler)
+
+    query = GetUserByID(user_id=user_id)
+
+    # Act
+    result = bus.process_query(query)
+
+    # Assert
+    assert result.user_id == user_id
+
+
+def test_process_invalid_query_return_rejection():
+    # Arrange
+    user_id = UUID('018f928b-5546-77e6-badf-3155de144924')
+
+    expected = 404
+    bus = ServiceBus()
+    handler = GetUserByIDQueryHandler()
+    bus.register(handler)
+
+    query = GetUserByID(user_id=user_id)
+
+    # Act
+    result = bus.process_query(query)
+
+    # Assert
+    assert result.status == expected
+
+
+def test_send_valid_command_return_command_acknowledgment():
+    # Arrange
+    bus = ServiceBus()
+    handler = RegisterUserCommandHandler()
+    bus.register(handler)
+
+    command = RegisterUser(
+        user_id=UUID('018f9284-769b-726d-b3bf-3885bf2ddd3c'),
+        name="John Doe Smith",
+        slug="john-doe-smith",
+    )
+
+    # Act
+    result = bus.send(command)
+
+    # Assert
+    assert result.status is CommandStatus.RECEIVED
+
+
+def test_send_valid_query_return_read_model():
+    # Arrange
+    user_id = UUID('018f9284-769b-726d-b3bf-3885bf2ddd3c')
+
+    bus = ServiceBus()
+    handler = GetUserByIDQueryHandler()
+    bus.register(handler)
+
+    query = GetUserByID(user_id=user_id)
+
+    # Act
+    result = bus.send(query)
+
+    # Assert
+    assert result.user_id == user_id
