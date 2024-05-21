fa995e0ce95bf1a5b3f3d4483fa99b50c9e885f8
Juan Luis Casanova <juanluiscr27@hotmail.com>
2024-05-20 22:31:28 -0400

    Fixed asdict method on ErrorDetail class

---
+++ tests/application/errors_test.py
@@ -0,0 +1,18 @@
+import json
+
+from sharedkernel.application.errors import ErrorDetail
+
+
+def test_error_detail_is_parsed_dictionary():
+    # Arrange
+    expected = {'loc': ['Users.CreateUser'],
+                'msg': 'First name is null or empty.',
+                'type': 'FirstName.NullOrEmpty', }
+
+    # Act
+    result = ErrorDetail(loc=["Users.CreateUser"],
+                         msg="First name is null or empty.",
+                         type="FirstName.NullOrEmpty", )
+
+    # Assert
+    assert result.asdict() == expected
