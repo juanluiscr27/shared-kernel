69841089712db73ed2cb484b6d4553a0df4a2d4f
Juan Luis Casanova <juanluiscr27@hotmail.com>
2024-05-23 22:18:09 -0400

    Defined API Error class

--- sharedkernel/api/errors.py
+++ sharedkernel/api/errors.py
@@ -1,7 +1,25 @@
+from sharedkernel.domain.errors import ServiceError
 from sharedkernel.infrastructure.errors import InfrastructureError
 
 
-class RequestMapperNotFound(InfrastructureError):
+class ApiError(ServiceError):
+    """API Error
+
+    Represents an error that occurred when processing requests or returning responses.
+
+    Args:
+        service: Service on which the error was raised.
+        message: Human readable string describing the exception.
+    """
+
+    def __init__(self, service: object, message: str):
+        super().__init__(message)
+        service_module = service.__module__
+        service_name = service.__name__
+        self.service = f"{service_module}.{service_name}"
+
+
+class RequestMapperNotFound(ApiError):
     """Request MapperNotFound Error
 
     Raised when no `RequestMapper` was found in the `MappingPipeline`.
