d94b1a1955d40d2e9baba391ffec8c361b4878d2
Juan Luis Casanova <juanluiscr27@hotmail.com>
2024-07-18 22:37:29 -0400

    Added tests for aggregate raise event and apply event methods

--- tests/domain/models_test.py
+++ tests/domain/models_test.py
@@ -1,5 +1,10 @@
 from dataclasses import dataclass
+from functools import singledispatchmethod
 
+import pytest
+
+from sharedkernel.domain.errors import UnknownEvent
+from sharedkernel.domain.events import DomainEvent
 from sharedkernel.domain.models import ValueObject, EntityID, Entity, Aggregate
 
 
@@ -26,11 +31,67 @@ class UserID(EntityID):
     value: int
 
 
+@dataclass(frozen=True)
+class UserRegistered(DomainEvent):
+    user_id: int
+    name: str
+
+
+@dataclass(frozen=True)
+class UserNameUpdated(DomainEvent):
+    user_id: int
+    new_name: str
+
+
+@dataclass(frozen=True)
+class UserLoggedIn(DomainEvent):
+    user_id: int
+    name: str
+
+
 class User(Aggregate[UserID]):
 
-    def __init__(self, user_id: UserID, name):
-        super().__init__(user_id, 0)
-        self.name: str = name
+    def __init__(self, user_id: UserID, version: int, name: str):
+        super().__init__(user_id, version)
+        self.name = name
+
+    @classmethod
+    def load(cls, user_id: UserID, version: int, name: str, events: tuple):
+        coach = cls(user_id=user_id, version=version, name=name)
+
+        for event in events:
+            coach._apply(event)
+
+        return coach
+
+    @classmethod
+    def register(cls, user_id: UserID, name: str):
+        user = cls(user_id=user_id, version=0, name=name)
+
+        user_registered = UserRegistered(user_id=user_id.value, name=name)
+
+        user._apply(user_registered)
+        user._raise_event(user_registered)
+
+        return user
+
+    def update_name(self, name: str) -> None:
+        name_updated = UserNameUpdated(user_id=self.id.value, new_name=name)
+
+        self._apply(name_updated)
+        self._raise_event(name_updated)
+
+    @singledispatchmethod
+    def _apply(self, event: DomainEvent) -> None:
+        super()._apply(event)
+
+    @_apply.register
+    def _when(self, event: UserRegistered) -> None:
+        self.name = event.name
+
+    @_apply.register
+    def _when(self, event: UserNameUpdated) -> None:
+        self.name = event.new_name
 
 
 def test_value_objects_with_same_value_are_equal():
@@ -111,9 +172,9 @@ def test_aggregates_with_same_id_are_equal():
     user_id = UserID(101)
 
     # Act
-    user1 = User(user_id=user_id, name="John Doe")
+    user1 = User(user_id=user_id, version=0, name="John Doe")
 
-    user2 = User(user_id=user_id, name="John Doe Jr.")
+    user2 = User(user_id=user_id, version=0, name="John Doe Jr.")
 
     # Assert
     assert user1 == user2
@@ -125,9 +186,53 @@ def test_aggregates_with_different_id_are_not_equal():
     user_id2 = UserID(102)
 
     # Act
-    user1 = User(user_id=user_id1, name="John Doe")
+    user1 = User(user_id=user_id1, version=0, name="John Doe")
 
-    user2 = User(user_id=user_id2, name="John Doe")
+    user2 = User(user_id=user_id2, version=0, name="John Doe")
 
     # Assert
     assert user1 != user2
+
+
+def test_aggregates_events_are_raised():
+    # Arrange
+    user_id = UserID(101)
+    expected = UserRegistered(user_id=101, name="John Doe")
+
+    # Act
+    user = User.register(user_id=user_id, name="John Doe")
+
+    result = user.changes[0]
+
+    # Assert
+    assert result == expected
+
+
+def test_aggregates_events_are_applied():
+    # Arrange
+    user_id = UserID(101)
+    user = User(user_id=user_id, version=1, name="John Doe")
+    new_name = "John Doe Smith"
+
+    # Act
+    user.update_name(new_name)
+
+    # Assert
+    assert user.name == new_name
+
+
+def test_aggregates_unknown_events_raise_error():
+    # Arrange
+    user_id = UserID(101)
+    events = (UserLoggedIn(user_id=101, name="John Doe"),)
+
+    expected = "Event 'UserLoggedIn' cannot be applied to 'User'"
+
+    # Act
+    with pytest.raises(UnknownEvent) as error:
+        _ = User.load(user_id=user_id, version=1, name="John Doe", events=events)
+
+    error_message = str(error.value)
+
+    # Assert
+    assert error_message == expected
