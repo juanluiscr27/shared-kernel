7e217a9dd0d40e2127f7c6b6a4fc997f4a3e792e
Juan Luis Casanova <juanluiscr27@hotmail.com>
2026-01-25 18:17:53 -0500

    Add docstring documentation to api module

--- sharedkernel/api/services.py
+++ sharedkernel/api/services.py
@@ -6,20 +6,42 @@ from sharedkernel.application.commands import Acknowledgement
 
 @dataclass
 class ElapsedTime:
+    """Represents the elapsed time of an operation.
+
+    Attributes:
+        value: The elapsed time in seconds.
+    """
     value: float
 
     @classmethod
-    def from_delta(cls, start: float, end: float):
+    def from_delta(cls, start: float, end: float) -> "ElapsedTime":
+        """Creates an ElapsedTime instance from a start and end time.
+
+        Args:
+            start: The start time in seconds.
+            end: The end time in seconds.
+        """
         return cls(end - start)
 
     @property
-    def milliseconds(self):
+    def milliseconds(self) -> int:
+        """Returns the elapsed time in milliseconds."""
         return round(self.value * 1000)
 
 
 class AckResponseModel:
+    """Utility class to create AckResponse objects."""
+
     @staticmethod
     def from_acknowledgement(ack: Acknowledgement) -> AckResponse:
+        """Creates an AckResponse from a command acknowledgement.
+
+        Args:
+            ack: The command acknowledgement.
+
+        Returns:
+            An AckResponse object.
+        """
         data = AckData(
             action=ack.action,
             entityId=ack.entity_id,
