b5503276dc212832c62038459e6404bd4cf4e0ac
Juan Luis Casanova <juanluiscr27@hotmail.com>
2026-01-25 18:17:12 -0500

    Add docstring documentation to domain module

--- sharedkernel/domain/models.py
+++ sharedkernel/domain/models.py
@@ -44,15 +44,18 @@ class Entity(Generic[TId]):
         self._id = entity_id
 
     @property
-    def id(self):
+    def id(self) -> TId:
+        """A strongly typed entity identifier."""
         return self._id
 
     @property
-    def qualname(self):
+    def qualname(self) -> str:
+        """Returns the qualified name of the entity class."""
         return self.__class__.__qualname__
 
     @property
-    def full_qualname(self):
+    def full_qualname(self) -> str:
+        """Returns the full qualified name of the entity class (module + name)."""
         return f"{self.__module__}.{self.qualname}"
 
     def __repr__(self):
@@ -94,18 +97,34 @@ class Aggregate(Entity[TId]):
 
     @property
     def version(self) -> int:
+        """The current version of the aggregate."""
         return self._version
 
     @property
     def changes(self) -> Tuple[DomainEvent, ...]:
+        """A tuple containing all the domain events that have occurred in the aggregate."""
         return tuple(self._events)
 
     def _apply(self, event: DomainEvent) -> None:
+        """Applies a domain event to the aggregate to change its state.
+
+        Args:
+            event: The domain event to apply.
+
+        Raises:
+            UnknownEvent: If the event is not handled by the aggregate.
+        """
         raise UnknownEvent(self, event)
 
     def _raise_event(self, event: DomainEvent) -> None:
+        """Raises a domain event, applying it to the aggregate and adding it to the changes' collection.
+
+        Args:
+            event: The domain event to raise.
+        """
         self._apply(event)
         self._events.append(event)
 
     def clear_events(self) -> None:
+        """Clears all the domain events in the aggregate."""
         self._events.clear()
